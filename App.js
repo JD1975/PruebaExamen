6cdae72ea46759dc5a4682971edd49c2feaf6222
Pancho02a <131419899+Pancho02a@users.noreply.github.com>
2024-02-28 14:14:02 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default App;
